fea706cbc34ecb6861d6c9fc5ad50d2656283d5a
Mia Chen <mia89926@gmail.com>
2020-05-10 23:51:16 +0800

    Revise generate_rubbish.js

--- generate_rubbish.js
+++ generate_rubbish.js
@@ -6,41 +6,34 @@ function getRandom(array) {
 
 function generateRubbish(options){
     //data
-    const task = {
-        engineer: ['加個按鈕','加新功能','切個版', '改一點 code'],
-        designer: ['畫一張圖', '改個 logo','順便幫忙設計一下','隨便換個設計'],
-        entrepreneur: ['週末加班', '要能賺錢','想個 business model','找 VC 募錢']
-      
+    const data = {
+        engineer: {
+          cn_role: "工程師",
+          task: ['加個按鈕','加新功能','切個版', '改一點 code'],
+        },
+
+        designer: {
+          cn_role: '設計師',
+          task: ['畫一張圖', '改個 logo','順便幫忙設計一下','隨便換個設計'],
+
+        },
+
+        entrepreneur: {
+          cn_role: "創業家",
+          task: ['週末加班', '要能賺錢','想個 business model','找 VC 募錢']
+        },
       }
     const phrase = ['很簡單','很容易','很快','很正常']
 
-    let role = ''
-    let roleTask = ''
-    let rolePhrase = getRandom(phrase)
-
-    // put tasks in taskBox by role
-    if (options === "engineer"){
-      role = '工程師'
-      roleTask = getRandom(task.engineer)
-    }
-
-    if (options === "designer"){
-      role = '設計師'
-      roleTask = getRandom(task.designer)
-    }
-
-    if (options === "entrepreneur"){
-      role = '創業家'
-      roleTask = getRandom(task.entrepreneur)
-    }
+    const roleData = data[options]
 
     // return error notice if no role selected
-    if (role === '') {
+    if (!roleData) {
       return '別再混了啦!!找個職業吧!!'
     }
 
     // return rubbish
-    let talkRubbish = `身為一個${role}，${roleTask}，一定${rolePhrase}~`
+    let talkRubbish = `身為一個${roleData.cn_role}，${getRandom(roleData.task)}，一定${getRandom(phrase)}~`
     return talkRubbish
 
 }
